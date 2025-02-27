09f2fe3f04bc5811364938282b42e973aba58e81
Uziel <urlaweblink@gmail.com>
2025-02-26 22:55:50 -0500

    Should be the final add button

--- bin/tr_Volume.py
+++ bin/tr_Volume.py
@@ -90,6 +90,11 @@ nanoHUB_flag = False
 if( 'HOME' in os.environ.keys() ):
     nanoHUB_flag = "home/nanohub" in os.environ['HOME']
 
+# Output widget to display run output 
+output_widget = widgets.Output()
+acc = widgets.Accordion(children=[output_widget]) #<- should in create a tab
+acc.set_title(0, 'Output')
+
 # # create the tabs, but don't display yet
 # about_tab = AboutTab()
 # config_tab = ConfigTab()
@@ -310,7 +315,7 @@ def run_done_func(s, rdir):
     # svg.update(rdir)
     sub.update(rdir)
 
-    animate_tab.gen_button.disabled = False
+    # animate_tab.gen_button.disabled = False
 
     # with debug_view:
     #     print('RDF DONE')
@@ -321,7 +326,7 @@ def run_sim_func(s):
     # with debug_view:
     #     print('run_sim_func')
 
-    animate_tab.gen_button.disabled = True
+    # animate_tab.gen_button.disabled = True
 
     # If cells or substrates toggled off in Config tab, toggle off in Plots tab
     if config_tab.toggle_svg.value == False:
@@ -392,59 +397,51 @@ def outcb(s):
 
 # Callback for the ("dumb") 'Run' button (without hublib.ui)
 def run_button_cb(s):
-#    with debug_view:
-#        print('run_button_cb')
+    """
+    Adding some doc just in case I forget 
 
-#    new_config_file = full_xml_filename
-    # print("new_config_file = ", new_config_file)
-#    write_config_file(new_config_file)
-    os.chdir(homedir)
+    The idea here is that we want to record the simulation output
+    in some separate window, kind of like a terminal window to do
+    so we use the output widget, and make as a "button/tab/dropdown" GUI
+    """
+    with output_widget:
+        output_widget.clear_output()  # Clear previous output
+        print("Running myproj ...")
 
-    os.system('rm -rf tmpdir*')
-    if os.path.isdir('tmpdir'):
-        # something on NFS causing issues...
-        tname = tempfile.mkdtemp(suffix='.bak', prefix='tmpdir_', dir='.')
-        shutil.move('tmpdir', tname)
-    os.makedirs('tmpdir')
+        # make sure we are where we started
+        os.chdir(homedir)
 
-    # write the default config file to tmpdir
-    new_config_file = "tmpdir/config.xml"  # use Path; work on Windows?
-    write_config_file(new_config_file)  
+        # remove any previous data
+        # NOTE: this dir name needs to match the <folder> in /data/<config_file.xml>
+        os.system('rm -rf tmpdir*')
+        if os.path.isdir('tmpdir'):
+            # something on NFS causing issues...
+            tname = tempfile.mkdtemp(suffix='.bak', prefix='tmpdir_', dir='.')
+            shutil.move('tmpdir', tname)
+        os.makedirs('tmpdir')
 
-    tdir = os.path.abspath('tmpdir')
-    os.chdir(tdir)  # operate from tmpdir; temporary output goes here.  may be copied to cache later
-    # svg.update(tdir)
-    # sub.update_params(config_tab)
-    sub.update(tdir)
+        # write the default config file to tmpdir
+        new_config_file = "tmpdir/config.xml"  # use Path; work on Windows?
+        write_config_file(new_config_file)
 
-    run_button.description = "WAIT..."
-    subprocess.run(["../bin/myproj", "config.xml"])
-    sub.max_frames.value = int(config_tab.tmax.value / config_tab.svg_interval.value)    # 42
-    run_button.description = "Run"
+        tdir = os.path.abspath('tmpdir')
+        os.chdir(tdir)  # operate from tmpdir; temporary output goes here. may be copied to cache later
+        # svg.update(tdir)
+        # sub.update_params(config_tab)
+        sub.update(tdir)
 
-    # os.chdir(homedir)
-
-    # # remove any previous data
-    # # NOTE: this dir name needs to match the <folder>  in /data/<config_file.xml>
-    # os.system('rm -rf tmpdir*')
-    # if os.path.isdir('tmpdir'):
-    #     # something on NFS causing issues...
-    #     tname = tempfile.mkdtemp(suffix='.bak', prefix='tmpdir_', dir='.')
-    #     shutil.move('tmpdir', tname)
-    # os.makedirs('tmpdir')
-
-    # # write the default config file to tmpdir
-    # new_config_file = "tmpdir/config.xml"  # use Path; work on Windows?
-    # write_config_file(new_config_file)  
-
-    # tdir = os.path.abspath('tmpdir')
-    # os.chdir(tdir)  # operate from tmpdir; temporary output goes here.  may be copied to cache later
-    # # svg.update(tdir)
-    # # sub.update_params(config_tab)
-    # sub.update(tdir)
-
-    # subprocess.Popen(["../bin/myproj", "config.xml"])
-
+        run_button.description = "WAIT..."
+        process = subprocess.Popen(["../bin/myproj", "config.xml"],
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE,
+                                   universal_newlines=True)
+        for line in process.stdout:
+            print(line, end="")
+        for line in process.stderr:
+            print(line, end="")
+        process.wait()
+        sub.max_frames.value = int(config_tab.tmax.value / config_tab.svg_interval.value)  # 42
+        run_button.description = "Run"
 
 #-------------------------------------------------
 if nanoHUB_flag:
@@ -505,13 +502,8 @@ if False:
     gui = widgets.VBox(children=[top_row, tabs, run_button.w])
     fill_gui_params(read_config.options['DEFAULT'])
 else:
-    # FINALLY FOUND IT
-    cpp_output = widgets.Output()
-    acc = widgets.Accordion(children=[cpp_output])
-    acc.set_title(0, 'Output')
-
     top_row = widgets.HBox(children=[tool_title])
-    gui = widgets.VBox(children=[top_row, tabs, run_button])
+    gui = widgets.VBox(children=[top_row, tabs, acc,run_button])
     fill_gui_params("data/PhysiCell_settings.xml")
 
 # pass in (relative) directory where output data is located
